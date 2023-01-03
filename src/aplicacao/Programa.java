0356c04136338372da73c6a60d1172fd0af9f95e
Thiago Vanzele <thiagovanzele@gmail.com>
2023-01-03 20:12:57 -0300

    Classe Departamento

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,14 @@
+package aplicacao;
+
+import negocio.entidades.Departamento;
+
+public class Programa {
+
+	public static void main(String[] args) {
+
+		Departamento obj = new Departamento(1, "Livros");
+		System.out.println(obj);
+		
+	}
+
+}
